4b6cfa1e79146df76a98cd7cf760fe9af0b618c5
KateKrechet <k.krechet@yandex.ru>
2021-12-16 21:59:55 +0300

    Classwork List2

---
+++ ClassList2/Source.cpp
@@ -0,0 +1,150 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+class List
+{
+	class Element
+	{
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
+			:Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	}*Head,*Tail;//Объявляем 2 казателя сразу же после описания класса
+	             //эти указатели будут переменными членами класса List
+	size_t size;//Размер списка
+public:
+	List()
+	{
+		Head = Tail = nullptr;//если список пуст, то голова и хвост указывают на 0
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~List()
+	{
+		cout << "LDestructor:\t" << this << endl;
+	}
+	//Adding elements
+	void push_front(int Data)
+	{
+		if (Head == nullptr && Tail==nullptr)
+		{
+			//когда в списке появляется первый элемент, он одновременно явяляется и головой, 
+			//и хвостом
+			Head = Tail = new Element(Data);
+			size++;
+			return;
+		}
+		Head = Head->pPrev = new Element(Data, Head);
+		size++;
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)return push_front(Data);
+			Tail = Tail->pNext = new Element(Data, nullptr, Tail);
+		size++;
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index >=size)
+		{
+			cout << "Выход за пределы списка!" << endl;
+			return;
+		}
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < Index; i++) Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPrev;
+		}
+		Element* New = new Element(Data);
+		New->pNext = Temp;
+		New->pPrev = Temp->pPrev;
+		Temp->pPrev->pNext = New;
+		Temp->pPrev = New;
+		size++;
+	}
+	//Removing elements
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+			size--;
+			return;
+		}
+		Head = Head->pNext;
+		delete Head->pPrev;
+		Head->pPrev = nullptr;
+		size--;
+
+	}
+	void pop_back()
+	{
+		if (Head==Tail) return pop_front();
+		Tail = Tail->pPrev;
+		delete Tail->pNext;
+		Tail->pNext = nullptr;
+		size--;
+	}
+	//Methods
+	void print()const
+	{
+		cout << "Head:\t" << Head << endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Tail:\t" << Tail << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
+	void reverse_print()const
+	{
+		cout << "Tail:\t" << Tail << endl;
+		for(Element*Temp=Tail;Temp;Temp=Temp->pPrev)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Head:\t" << Head << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_back(rand() % 100);
+	}
+	list.print();
+	list.reverse_print();
+	cout << "\n-----------------------------------------------------------\n";
+	/*list.pop_front();
+	list.pop_back();*/
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(value, index);
+	list.print();
+	list.reverse_print();
+
+}
