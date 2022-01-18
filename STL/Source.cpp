aa843e42e899186a6b04e239cebe477111c94ba0
KateKrechet <k.krechet@yandex.ru>
2022-01-18 20:18:16 +0300

    Vector DONE

--- STL/Source.cpp
+++ STL/Source.cpp
@@ -5,10 +5,18 @@ using namespace std;
 
 #define tab "\t"
 //#define STL_ARRAY
-//#define VECTOR
-#define VECTOR_HW
+#define VECTOR
+//#define VECTOR_HW
 
-void PrintVector(vector<int> vec, vector<int>::iterator i_iterator);
+void PrintVector(vector<int> vec);
+template<typename T> void print(const vector<T>& vec)
+{
+	for (typename vector<T>::const_iterator it = vec.begin(); it != vec.end(); it++)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+}
 
 void main()
 {
@@ -48,13 +56,41 @@ void main()
 	cout << "Capacity:  " << vec.capacity() << endl;//есть только у вектора
 	cout << "MaxSize:  " << vec.max_size() << endl;
 	cout << sizeof(vec) << endl;
+	
+	//vector<vector<int> >vec2;
+	//cout << "Size:  " << vec2.size() << endl;
+	//cout << "Capacity:  " << vec2.capacity() << endl;//есть только у вектора
+	//cout << "MaxSize:  " << vec2.max_size() << endl;
+	//cout << sizeof(vec2) << endl;
 
+	int index;
+	int value;
+	int count;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите количество добавлений: "; cin >> count;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	
+	if (index < vec.size())vec.insert(vec.begin() + index,count, value);
+	else cout << "Error: out of range " << endl;
+	print(vec);
 
-	vector<vector<int> >vec2;
-	cout << "Size:  " << vec2.size() << endl;
-	cout << "Capacity:  " << vec2.capacity() << endl;//есть только у вектора
-	cout << "MaxSize:  " << vec2.max_size() << endl;
-	cout << sizeof(vec2) << endl;
+	std::vector<int>powers = { 1024,2048,4096,8192,16384 };
+
+	vec.insert(vec.begin() +4 , powers.begin(), powers.end());
+	for (int i : vec) cout << i << tab; cout << endl;
+
+	vec.insert(vec.begin() + 8, { 256,384,512,768 });
+	for (int i : vec) cout << i << tab; cout << endl;
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	cout << "Введите количество удалений: "; cin >> count;
+	vec.erase(vec.begin() + index, vec.begin() + index+count);
+	print(vec);
+	cout << "\n----------------------------------------------------------\n";
+	vec.swap(powers);
+	print(vec);
+	print(powers);
+
 #endif // VECTOR
 #ifdef VECTOR_HW
 	vector<int> vec = { 0,1,1,2,3,5,8,13,21,34,55,89,144,232 };
@@ -63,40 +99,31 @@ void main()
 	cout << "MaxSize:  " << vec.max_size() << endl;
 	cout << sizeof(vec) << endl;
 	int* data = vec.data();
-	vector<int>::iterator i_iterator;
-	for (i_iterator = vec.begin();
-		i_iterator != vec.end(); i_iterator++)
-	{
-		cout << *(i_iterator) << '\t';
-	}
-	cout << endl;
+	vector<int>::iterator i_iterator = vec.begin();
+	PrintVector(vec);
 	int value;
 	int index;
 	cout << "Введите добавляемое значение: "; cin >> value;
 	cout << "Введите позицию: "; cin >> index;
 	if (index > vec.size())return;
 	//вставка заданного элемента в заданную позицию
-	vec.insert(i_iterator - vec.size() + index, value);
+	vec.insert(vec.begin() + index, value);
 	cout << "Вектор с добавленным значением: " << endl;
-	for (i_iterator = vec.begin(); i_iterator != vec.end(); i_iterator++)
-
-	{
-		cout << *(i_iterator) << '\t';
-	}
-	cout << endl;
+	PrintVector(vec);
 	cout << "Введите позицию для удаления: "; cin >> index;
 	if (index > vec.size())return;
 	//удаление заданного элемента по заданной позиции
-	vec.erase(i_iterator - vec.size() + index);
+	i_iterator = vec.end();
+	vec.erase(vec.begin() + index);
 	cout << "Вектор с удаленным значением: " << endl;
-	PrintVector(vec, i_iterator);
+	PrintVector(vec);
 
 #endif // VECTOR_HW
 }
 
-void PrintVector(vector<int> vec, vector<int>::iterator i_iterator)
+void PrintVector(vector<int> vec)
 {
-	for (i_iterator = vec.begin(); i_iterator != vec.end(); i_iterator++)
+	for (vector<int>::iterator i_iterator = vec.begin(); i_iterator != vec.end(); i_iterator++)
 
 	{
 		cout << *(i_iterator) << '\t';
